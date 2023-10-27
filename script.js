b0df1628233f98a8ba29f96b2f2464aa863473eb
Kervin Espirito Santo <57775586+kervines@users.noreply.github.com>
2023-10-27 02:11:15 +0000

    implementaçao do `script.js` e modificações no `index.html` e `style.css`

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const textArea = document.querySelector("#text-area");
+const totalChar = document.querySelector(".total-char");
+const remaining = document.querySelector(".remaining");
 
 textArea.addEventListener("keydown", (event) => {
-  console.log(event.currentTarget.value.length);
+  const maxCharater = 150;
+  totalChar.innerText = event.currentTarget.value.length;
+  remaining.innerText = maxCharater - totalChar.innerText;
+
+  if (remaining.innerText == 130) {
+  }
 });
